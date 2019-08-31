f60d31774c4fd99320d57d7bb690533158023e47
Raymond Li <hi@raymond.li>
2019-08-30 23:28:38 -0400

    Bug fixes

--- convert.sh
+++ convert.sh
@@ -8,6 +8,11 @@ indir=$(dirname "${VAR}")
 outdir=$indir
 scriptpath="$( cd "$(dirname "$0")" ; pwd -P )"
 
+show_help () {
+	echo -e "Welcome to convertcursor, which helps you convert your Windows cursors to Linux.\nImageMagick is the only required dependency.\nTo use this program, do:\nconvertcursor [options]\nThe options are as follows:\n-h\tShows this help menu\n-f [file]\tConvert a file\n-F [folder]\tConvert a folder
+\n-o [path]\tOutput to directory (default is the original directory of the file) (make sure you omit the ending '/')"\nNote: To convert multiple files or folders, simply use -f or -F multiple times, like so:\nconvertcursor -f file1 -f file2 -F folder1 -F folder2 -o /path/to/output/folder
+}
+
 while getopts ":hf:F:o:" opt; do
 	case $opt in
 	h)
@@ -90,8 +95,3 @@ done
 shift $((OPTIND-1))
 
 [ "${1:-}" = "--" ] && shift
-
-show_help () {
-	echo -e "Welcome to convertcursor, which helps you convert your Windows cursors to Linux.\nImageMagick is the only required dependency.\nTo use this program, do:\nconvertcursor [options]\nThe options are as follows:\n-h\tShows this help menu\n-f [file]\tConvert a file\n-F [folder]\tConvert a folder
-\n-o [path]\tOutput to directory (default is the original directory of the file) (make sure you omit the ending '/')"\nNote: To convert multiple files or folders, simply use -f or -F multiple times, like so:\nconvertcursor -f file1 -f file2 -F folder1 -F folder2 -o /path/to/output/folder
-}
